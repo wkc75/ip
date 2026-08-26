4eebd3c4452630b6842dd08e8c0783b865bf50bd
wkc <wongkinchong75@gmail.com>
2026-08-26 21:17:16 +0800

    ZhangWei.java: Add tasks to a list and display them on request

--- src/main/java/ZhangWei.java
+++ src/main/java/ZhangWei.java
@@ -1,6 +1,29 @@
 import java.util.Scanner;
 
+/**
+ * ZhangWei
+ * This is a chatbot named ZhangWei
+ */
 public class ZhangWei {
+
+    private static final int MAX_TASKS = 100;
+    private static final String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
+    // add task to tasks
+    private static void addTask(String task) {
+        tasks[taskCount] = task;
+        taskCount++;
+        System.out.println("added: " + task);
+    }
+
+    // list down all tasks
+    private static void listTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+    }
+
     public static void main(String[] args) {
         // Each backslash in the ASCII art must be written as \\ in a Java
         // string literal, because \ starts an escape sequence.
@@ -16,14 +39,18 @@ public class ZhangWei {
         System.out.println("What can I do for you?");
 
         Scanner scan = new Scanner(System.in);
-        boolean isEcho = true;
-        while (isEcho) {
+
+        boolean isRunning = true;
+
+        while (isRunning) {
             String input = scan.nextLine();
             if (input.equals("bye")) {
-                isEcho = false;
+                isRunning = false;
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (input.equals("list")) {
+                listTasks();
             } else {
-                System.out.println(input);
+                addTask(input);
             }
         }
         scan.close();
