71ce9761b976cae847f9bb201f888e38eef38e65
wkc <wongkinchong75@gmail.com>
2026-08-26 20:21:57 +0800

    Rename chatbot Duke to ZhangWei; Completed greet and exit

---
+++ src/main/java/ZhangWei.java
@@ -0,0 +1,17 @@
+public class ZhangWei {
+    public static void main(String[] args) {
+        // Each backslash in the ASCII art must be written as \\ in a Java
+        // string literal, because \ starts an escape sequence.
+        String banner = " ______                     __        __   _ \n"
+                + "|__  / |__   __ _ _ __   __ \\ \\      / /__(_)\n"
+                + "  / /| '_ \\ / _` | '_ \\ / _` \\ \\ /\\ / / _ \\ |\n"
+                + " / /_| | | | (_| | | | | (_| |\\ V  V /  __/ |\n"
+                + "/____|_| |_|\\__,_|_| |_|\\__, | \\_/\\_/ \\___|_|\n"
+                + "                        |___/\n";
+        System.out.println(banner);
+
+        System.out.println("Hello! I'm ZhangWei.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
